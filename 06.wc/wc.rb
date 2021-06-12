7228a8cf816ab2f0713aadb3ede75ad385fe4b77
Gen Tamura <gen.tamura84@gmail.com>
2021-06-12 17:32:09 +0900

    Add padding and one line format

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -41,14 +41,27 @@ pattern = if ARGV.empty?
 
 files = Dir.glob(pattern).sort
 
+BASE_PADDING = 8
+
 files.each do |f|
   file = File.new(f)
-  line_num = file.each_line.count # lines
+  line = file.each_line.count # lines
 
   file = File.new(f)
-  word_num = file.each_line.reduce { |result, s| result << s }.split(' ').count # words
+  word = file.each_line.reduce { |result, s| result << s }.split.count # words
 
-  byte_num = file.size # byte
+  byte = file.size # byte
 
-  p line_num, word_num, byte_num
+  line_len = line.to_s.length
+  word_len = word.to_s.length
+  byte_len = byte.to_s.length
+
+  line_padding = line_len > BASE_PADDING ? line_len + 1 : BASE_PADDING
+  word_padding = word_len > BASE_PADDING ? word_len + 1 : BASE_PADDING
+  byte_padding = byte_len > BASE_PADDING ? byte_len + 1 : BASE_PADDING
+
+  print line.to_s.rjust(line_padding)
+  print word.to_s.rjust(word_padding)
+  print byte.to_s.rjust(byte_padding)
+  puts " #{file.to_path}"
 end
