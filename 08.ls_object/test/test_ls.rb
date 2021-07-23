92d7d43f3045b5edd42dae9b9d82a4e834e38999
Gen Tamura <gen.tamura84@gmail.com>
2021-07-23 12:01:10 +0900

    Add tests with all and reverse and three options

--- 08.ls_object/test/test_ls.rb
+++ 08.ls_object/test/test_ls.rb
@@ -73,6 +73,63 @@ class LsCommandTest < Minitest::Test
       -rw-r--r--    1 gentamura  staff  306678  7 22 16:31 yarn.lock
     TEXT
 
-    assert_equal expected, exec_ls(TARGET_PATHNAME, long: true)
+    assert_equal expected, exec_ls(TARGET_PATHNAME, window_width: 180, long: true)
+  end
+
+  def test_exec_ls_with_all
+    expected = <<~TEXT.chomp
+      .                  .gitattributes     Gemfile.lock       babel.config.js    db                 package.json       test
+      ..                 .gitignore         README.md          bin                lib                postcss.config.js  tmp
+      .browserslistrc    .ruby-version      Rakefile           config             log                public             vendor
+      .git               Gemfile            app                config.ru          node_modules       storage            yarn.lock
+    TEXT
+
+    assert_equal expected, exec_ls(TARGET_PATHNAME, window_width: 180, all: true)
+  end
+
+  def test_exec_ls_with_reverse
+    expected = <<~TEXT.chomp
+      yarn.lock          test               postcss.config.js  log                config.ru          babel.config.js    README.md
+      vendor             storage            package.json       lib                config             app                Gemfile.lock
+      tmp                public             node_modules       db                 bin                Rakefile           Gemfile
+    TEXT
+
+    assert_equal expected, exec_ls(TARGET_PATHNAME, window_width: 180, reverse: true)
+  end
+
+  def test_exec_ls_with_long_and_all_and_reverse
+    expected = <<~TEXT.chomp
+      total 704
+      -rw-r--r--    1 gentamura  staff  306678  7 22 16:31 yarn.lock
+      drwxr-xr-x    3 gentamura  staff      96  7 22 16:30 vendor
+      drwxr-xr-x    8 gentamura  staff     256  7 22 16:30 tmp
+      drwxr-xr-x   12 gentamura  staff     384  7 22 16:30 test
+      drwxr-xr-x    3 gentamura  staff      96  7 22 16:30 storage
+      drwxr-xr-x    9 gentamura  staff     288  7 22 16:30 public
+      -rw-r--r--    1 gentamura  staff     224  7 22 16:30 postcss.config.js
+      -rw-r--r--    1 gentamura  staff     375  7 22 16:31 package.json
+      drwxr-xr-x  691 gentamura  staff   22112  7 22 16:31 node_modules
+      drwxr-xr-x    4 gentamura  staff     128  7 22 16:30 log
+      drwxr-xr-x    4 gentamura  staff     128  7 22 16:30 lib
+      drwxr-xr-x    3 gentamura  staff      96  7 22 16:30 db
+      -rw-r--r--    1 gentamura  staff     160  7 22 16:30 config.ru
+      drwxr-xr-x   18 gentamura  staff     576  7 22 16:30 config
+      drwxr-xr-x   10 gentamura  staff     320  7 22 16:30 bin
+      -rw-r--r--    1 gentamura  staff    1668  7 22 16:30 babel.config.js
+      drwxr-xr-x   11 gentamura  staff     352  7 22 16:30 app
+      -rw-r--r--    1 gentamura  staff     227  7 22 16:30 Rakefile
+      -rw-r--r--    1 gentamura  staff     374  7 22 16:30 README.md
+      -rw-r--r--    1 gentamura  staff    5489  7 22 16:30 Gemfile.lock
+      -rw-r--r--    1 gentamura  staff    2196  7 22 16:30 Gemfile
+      -rw-r--r--    1 gentamura  staff       6  7 22 16:30 .ruby-version
+      -rw-r--r--    1 gentamura  staff     840  7 22 16:30 .gitignore
+      -rw-r--r--    1 gentamura  staff     327  7 22 16:30 .gitattributes
+      drwxr-xr-x    9 gentamura  staff     288  7 22 16:30 .git
+      -rw-r--r--    1 gentamura  staff       9  7 22 16:30 .browserslistrc
+      drwxr-xr-x    3 gentamura  staff      96  7 22 16:30 ..
+      drwxr-xr-x   28 gentamura  staff     896  7 22 16:30 .
+    TEXT
+
+    assert_equal expected, exec_ls(TARGET_PATHNAME, window_width: 180, long: true, all: true, reverse: true)
   end
 end
