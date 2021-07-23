6d42184bc4194bc49095d21c731d74843b5cf69b
Gen Tamura <gen.tamura84@gmail.com>
2021-07-23 17:01:05 +0900

    Move stat details from main to file calss

--- 08.ls_object/src/file.rb
+++ 08.ls_object/src/file.rb
@@ -7,11 +7,68 @@ module Ls
     end
 
     def basename
-      ::File.basename(@file_name)
+      @basename ||= ::File.basename(@file_name)
     end
 
     def stat
-      ::File.stat(@file_name)
+      @stat ||= ::File.stat(@file_name)
+    end
+
+    def stat_detail(nlink: 0, user: 0, group: 0, size: 0)
+      fs = stat
+
+      row = "#{ftype_and_permission(fs.mode.to_s(8))}  " # ファイルタイプ アクセス権
+      row += "#{fs.nlink.to_s.rjust(nlink)} " # ハードリンク数
+      row += "#{user_name(fs.uid).rjust(user)}  " # 所有者名
+      row += "#{group_name(fs.gid).rjust(group)}  " # グループ名
+      row += "#{fs.size.to_s.rjust(size)} " # バイト数
+      row += "#{format_time(fs.mtime)} #{basename}" # 更新日時（または更新年月日） ファイル名
+
+      row
+    end
+
+    private
+
+    def ftype(octal_str)
+      {
+        '10' => 'p', # FIFO
+        '20' => 'c', # Character device
+        '40' => 'd', # Directory
+        '60' => 'b', # Block device
+        '100' => '-', # Regular file
+        '120' => 'l', # Symbolic link
+        '140' => 's' # Socket
+      }[octal_str]
+    end
+
+    def permission(permission_str)
+      {
+        '1' => '--x',
+        '2' => '-w-',
+        '4' => 'r--',
+        '5' => 'r-x',
+        '6' => 'rw-',
+        '7' => 'rwx'
+      }[permission_str]
+    end
+
+    def ftype_and_permission(octal_str)
+      permission = octal_str.slice!(-3, 3)
+
+      ftype(octal_str) + permission.split('').map { |d| permission(d) }.join
+    end
+
+    def user_name(uid)
+      Etc.getpwuid(uid).name
+    end
+
+    def group_name(gid)
+      Etc.getgrgid(gid).name
+    end
+
+    def format_time(time)
+      format = time.year == Time.now.year ? '%_m %_d %H:%M' : '%_m %_d  %Y'
+      time.strftime(format)
     end
   end
 end
--- 08.ls_object/src/main.rb
+++ 08.ls_object/src/main.rb
@@ -8,48 +8,6 @@ require 'io/console/size'
 
 require_relative './file'
 
-def ftype(octal_str)
-  {
-    '10' => 'p', # FIFO
-    '20' => 'c', # Character device
-    '40' => 'd', # Directory
-    '60' => 'b', # Block device
-    '100' => '-', # Regular file
-    '120' => 'l', # Symbolic link
-    '140' => 's' # Socket
-  }[octal_str]
-end
-
-def permission(permission_str)
-  {
-    '1' => '--x',
-    '2' => '-w-',
-    '4' => 'r--',
-    '5' => 'r-x',
-    '6' => 'rw-',
-    '7' => 'rwx'
-  }[permission_str]
-end
-
-def ftype_and_permission(octal_str)
-  permission = octal_str.slice!(-3, 3)
-
-  ftype(octal_str) + permission.split('').map { |d| permission(d) }.join
-end
-
-def user_name(uid)
-  Etc.getpwuid(uid).name
-end
-
-def group_name(gid)
-  Etc.getgrgid(gid).name
-end
-
-def format_time(time)
-  format = time.year == Time.now.year ? '%_m %_d %H:%M' : '%_m %_d  %Y'
-  time.strftime(format)
-end
-
 def long_option(files, result)
   padding_hash = {
     nlink: 0,
@@ -63,11 +21,14 @@ def long_option(files, result)
   files.each do |file|
     fs = file.stat
 
+    user_name = Etc.getpwuid(fs.uid).name
+    group_name = Etc.getgrgid(fs.gid).name
+
     # Set padding count
-    padding_hash[:nlink] = fs.nlink.to_s.length if fs.nlink.to_s.length > padding_hash[:nlink]
-    padding_hash[:user] = user_name(fs.uid).length if user_name(fs.uid).length > padding_hash[:user]
-    padding_hash[:group] = group_name(fs.gid).length if group_name(fs.gid).length > padding_hash[:group]
-    padding_hash[:size] = fs.size.to_s.length if fs.size.to_s.length > padding_hash[:size]
+    padding_hash[:nlink] = fs.nlink.to_s.size if fs.nlink.to_s.size > padding_hash[:nlink]
+    padding_hash[:user] = user_name.size if user_name.size > padding_hash[:user]
+    padding_hash[:group] = group_name.size if group_name.size > padding_hash[:group]
+    padding_hash[:size] = fs.size.to_s.size if fs.size.to_s.size > padding_hash[:size]
 
     # Reduce total blocks
     total_block_count += fs.blocks
@@ -77,17 +38,7 @@ def long_option(files, result)
   result << "total #{total_block_count}"
 
   files.each do |file|
-    fs = file.stat
-
-    long_list = "#{ftype_and_permission(fs.mode.to_s(8))}  " # ファイルタイプ アクセス権
-    long_list += "#{fs.nlink.to_s.rjust(padding_hash[:nlink])} " # ハードリンク数
-    long_list += "#{user_name(fs.uid).rjust(padding_hash[:user])}  " # 所有者名
-    long_list += "#{group_name(fs.gid).rjust(padding_hash[:group])}  " # グループ名
-    long_list += "#{fs.size.to_s.rjust(padding_hash[:size])} " # バイト数
-    long_list += "#{format_time(fs.mtime)} #{file.basename}" # 更新日時（または更新年月日） ファイル名
-
-    # puts result
-    result << long_list
+    result << file.stat_detail(**padding_hash)
   end
 end
 
@@ -95,7 +46,6 @@ def exec_ls(pathname, window_width: IO.console_size[1], reverse: false, long: fa
   flags = all ? File::FNM_DOTMATCH : 0
 
   file_names = Dir.glob(pathname.join('*'), flags).sort
-  file_names = file_names.reverse if reverse
 
   max_file_name_size = 0
 
